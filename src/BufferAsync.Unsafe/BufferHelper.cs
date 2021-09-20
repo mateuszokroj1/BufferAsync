818fd4cb90ed0e4fe189b18f1811d7ad275ef890
mateuszokroj1 <mateuszokroj1@gmail.com>
2021-09-21 01:13:12 +0200

    Created basic classes.

---
+++ src/BufferAsync.Unsafe/BufferHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace BufferAsync.Unsafe
+{
+    public static unsafe class BufferHelper
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        /// <exception cref="OutOfMemoryException"/>
+        public static Memory<byte> CreateFirstBlock(int size)
+        {
+            var pointer = Marshal.AllocHGlobal(size);
+
+            
+        }
+
+        public static void InitBlock(ref Memory<byte> block)
+        {
+            if (block.IsEmpty)
+                throw new ArgumentException("Memory is empty.");
+
+            var handle = block.Pin();
+            System.Runtime.CompilerServices.Unsafe.InitBlock(handle.Pointer, 0, (uint)block.Length);
+        }
+    }
+}
---
+++ src/BufferAsync/Buffer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BufferAsync
+{
+    public sealed class Buffer : IBuffer
+    {
+        #region Constructor
+
+        public Buffer(long maxAvailableSize)
+        {
+
+        }
+
+        public Buffer() : this(0) { }
+
+        #endregion
+
+        #region Fields
+
+        private Memory<byte> buffer = Memory<byte>.Empty;
+
+        private readonly SemaphoreSlim locker = new SemaphoreSlim(1, 1);
+        private readonly IDictionary<Guid, Memory<byte>> blocksCollection = new Dictionary<Guid, Memory<byte>>();
+
+        #endregion
+
+        #region Properties
+
+        public long MaxAvailableSize { get; }
+
+        public IEnumerable<KeyValuePair<Guid, Memory<byte>>> Blocks => blocksCollection.AsEnumerable();
+
+        #endregion
+
+        #region Methods
+
+        public async ValueTask<Guid> CreateMemoryBlock(long size, bool initWithZeros = false)
+        {
+
+        }
+
+        public async ValueTask<Guid> CreateMemoryBlockWhenIsAvailable()
+        {
+
+        }
+
+        public async void Dispose() => await DisposeAsync();
+
+        public ValueTask DisposeAsync()
+        {
+            return ValueTask.CompletedTask;
+        }
+
+        #endregion
+    }
+}
